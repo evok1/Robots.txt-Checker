296a2fc7ca2bca3f3710110627cf1b01e129102e
evok1 <loic.j.julien@gmail.com>
2016-08-19 22:50:27 +0200

    deploy test Heroku

--- app.js
+++ app.js
@@ -6,17 +6,12 @@
 // 1. Creating a web sever
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const port =  process.en.PORT || 1337 ;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 /*
