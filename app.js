f4f0a3c4d065a3987f161d23688ada37929fd61a
evok1 <loic.j.julien@gmail.com>
2016-08-19 21:19:50 +0200

    first version

---
+++ app.js
@@ -0,0 +1,61 @@
+// Problem : Sometimes, developpement push modified robots.txt and Google gets blocked by disallow in robots.txt files.
+// Solution : Checking each morning for good robots.txt settings of numerous websites, with node js.
+
+// 1. Creating a web sever
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+/*
+
+// Declaring variables
+var siteUrl = "http://www.loicjulien.fr/"
+
+// Getting robots.txt
+// console.log(">> Getting robots.txt");
+var request = http.get(siteUrl + "robots.txt", function(response) {
+
+	var robotsTxtContent = "Robots.txt : \n" ;
+	var robotsTxtData = "";
+
+	// Checking status code
+	// console.log(response.statusCode);
+
+	// Read the data
+	// console.log(">> Reading robots.txt");
+	
+	response.on("data", function (chunck) {
+		robotsTxtContent  = robotsTxtContent + chunck;
+	});
+
+	response.on("end", function () {
+		// console.log(robotsTxtContent);
+	});
+
+	// Checking if robots.txt is Google friendly
+	var robotsTxtGoogleFriendly = robotsTxtContent.includes("Disallow: /");
+	console.log(robotsTxtGoogleFriendly);
+
+});
+
+request.on("error", function(error) {
+	console.error(error.message);
+});
+
+/*
+
+User-agent: *
+Disallow: /
+
+*/
