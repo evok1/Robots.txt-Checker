fa22b17e4ba7ed6aca6d46729ee3307551d3322e
evok1 <loic.j.julien@gmail.com>
2016-08-21 22:16:05 +0200

    css

--- Robots-Checker.php
+++ Robots-Checker.php
@@ -4,15 +4,9 @@ Not a developer in real life but like to write some code sometimes.
 Say hello at loic.j.julien@gmail.com
 -->
 
-<!--
-Allow: /
-Disallow: /
--->
-
 <?php
 
 // $robots = file_get_contents('http://www.loicjulien.fr/robots.txt') ;
-
 $robots1 = 'Sitemap: http://www.loicjulien.fr/sitemap_index.xml User-agent: * Allow: / Disallow: /dossierpourrit' ;
 $robots2 = 'Sitemap: http://www.loicjulien.fr/sitemap_index.xml User-agent: * Allow: / Disallow: /' ;
 $robots3 = 'User-agent: * Allow: / Disallow: /dossierpourit' ;
@@ -22,81 +16,31 @@ $robots6 = 'Sitemap: http://www.loicjulien.fr/sitemap_index.xml User-agent: *' ;
 
 function robotsCheck ($dataToCheck)
 {
-echo '<br>', 'Testing ', $dataToCheck, '<br>' ;
+	$sitemap ;
+	$allow ;
+	$disallow ;
 
-$sitemap ;
-$allow ;
-$disallow ;
+	// Recherche d'un sitemap
+	if (preg_match('/Sitemap:/', $dataToCheck))
+		$sitemap = true ;
+		else $sitemap = false ;
 
-// Recherche d'un sitemap
-if (preg_match('/Sitemap:/', $dataToCheck))
-	$sitemap = true ;
-	//echo 'Sitemap présent', '<br>' ;
-	else $sitemap = false ;
+	// Recherche d'un Allow: /
+	if (preg_match('/Allow: \/$/', $dataToCheck))
+		$allow = false ;
+		else $allow = true ;
 
-// Recherche d'un Allow: /
-if (preg_match('/Allow: \/$/', $dataToCheck))
-	$allow = false ;
-	//echo 'N\'est pas présent', '<br>' ;
-	else $allow = true ;
+	// Recherche d'un Disallow: /
+	if (preg_match('/Disallow: \/$/', $dataToCheck))
+		$disallow = true ;
+		else $disallow = false ;
 
-// Recherche d'un Disallow: /
-if (preg_match('/Disallow: \/$/', $dataToCheck))
-	$disallow = true ;
-	//echo 'Disallow: / n\'est pas présent', '<br>' ;
-	else $disallow = false ;
+	if ($sitemap == true and $allow == true and $disallow == false)
+		$sitemapStatus = 'OK' ;
+		else $sitemapStatus = 'Need modification !' ;
 
-if ($sitemap == true and $allow == true and $disallow == false)
-	$sitemapStatus = 'OK' ;
-	else $sitemapStatus = 'Need modification !' ;
-
-echo 'Robots.txt status : ', $sitemapStatus ,' sitemap :', $sitemap, ' allow :', $allow, ' disallow :', $disallow, '<br>' ;
+	echo 'Robots.txt status : ', $sitemapStatus ,' sitemap :', $sitemap, ' allow :', $allow, ' disallow :', $disallow, '<br>' ;
 }
-
-robotsCheck($robots1) ;
-robotsCheck($robots2) ;
-robotsCheck($robots3) ;
-robotsCheck($robots4) ;
-robotsCheck($robots5) ;
-robotsCheck($robots6) ;
-
-/*
-$mystring = 'abc';
-$findme   = 'a';
-$pos = strpos($mystring, $findme);
- 
-// Notez notre utilisation de ===.  == ne fonctionnerait pas comme attendu
-// car la position de 'a' est la 0-ième (premier) caractère.
-if ($pos === false) {
-	echo "La chaîne '$findme' ne se trouve pas dans la chaîne '$mystring'";
-} else {
-	echo "La chaine '$findme' a été trouvée dans la chaîne '$mystring'";
-	echo " et débute à la position $pos";
-}
-*/
-
-/*
-if (preg_match('/Sitemap:/', $robots))
-	$robotSitemap = 'Sitemap présent' ;
-if (preg_match('/Allow: \//', $robots))
-    	$robotAllow = 'Allow: / présent' ;
-if (preg_match('/Disallow: \//', $robots))
-	$robotDisallow = 'Disallow: / présent' ;
-*/
-
-/*
-function searchRobotsTxt ($data)
-{
-if (preg_match('/Sitemap:/', $data))
-	$robotSitemap = 'Sitemap présent' ;
-if (preg_match('/Allow: \//', $data))
-    	$robotAllow = 'Allow: / présent' ;
-if (preg_match('/Disallow: \//', $data))
-	$robotDisallow = 'Disallow: / présent' ;
-}
-*/
-
-// searchRobotsTxt($robots) ;
 
 ?>
 
@@ -105,15 +49,15 @@ if (preg_match('/Disallow: \//', $data))
 <head>
 	<meta charset="utf-8">
 	<title>Robots.txt Checker : never be de-indexed by Google</title>
-
 	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script> -->
-
 	<!-- Bootstrap css & js -->
 	<!-- 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	-->
+
+	<link rel="stylesheet" href="css/app.css">
 
 </head>
 
@@ -127,7 +71,14 @@ if (preg_match('/Disallow: \//', $data))
 	</div>
 	<div class="row">
 		<div class="col-xs-offset-2 col-xs-8 col" id="content">
-
+		<?php
+		robotsCheck($robots1) ;
+		robotsCheck($robots2) ;
+		robotsCheck($robots3) ;
+		robotsCheck($robots4) ;
+		robotsCheck($robots5) ;
+		robotsCheck($robots6) ;
+		?>
 		</div>
 		<div class="col-xs-2">
 	</div>
