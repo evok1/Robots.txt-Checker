ed138beb5c86d84206a636ec7275e1088ba6ade4
Loïc JULIEN <loic.j.julien@gmail.com>
2017-04-01 12:50:34 +0200

    updating app.php for better checking

--- php/app.php
+++ php/app.php
@@ -1,7 +1,7 @@
 <?php 
 
 // Sites to Check
-$sites = ['https://www.google.fr','https://www.youtube.com','https://www.apple.com/','https://www.microsoft.com','http://barthloc.com','http://juku.fr'] ;
+$sites = ['https://www.google.fr','https://www.youtube.com','https://www.apple.com/','https://www.microsoft.com'] ;
 
 // Variable
 $site_en_ligne ;
@@ -9,19 +9,27 @@ $robots_txt_friendly ;
 $meta_robot_friendly ;
 
 function checkingSite ($data) {
-
-	foreach ($data as &$url) {
+	foreach ($data as $url) {
 
 		// ====================================================================================
 		// Checking Http Status
 
 		$headers = get_headers($url) ;
 		$server_status = $headers[0] ;
-		// echo $server_up ;
 		if (strpos($server_status, '200') !== false) {
  			$site_en_ligne = "<div class=\"bg-success message text-center \">200</div>" ;
 		} elseif (strpos($server_status, '301') !== false) {
-			$site_en_ligne = "<div class=\"bg-warning message text-center \">301</div>" ;
+			$server_status = $headers[18] ;
+			$url = substr($headers[9], 10);
+				if (strpos($server_status, '200') !== false) {
+	 				$site_en_ligne = "<div class=\"bg-success message text-center \">200</div>" ;
+				} elseif (strpos($server_status, '301') !== false) {
+					$url = $url + ': 301 chain. Need correction' ;
+					$site_en_ligne = "<div class=\"bg-warning message text-center \">301 chain</div>" ;
+				} else {
+					$site_en_ligne = "<div class=\"bg-danger message text-center \">ERROR</div>" ;
+				}
+			// $site_en_ligne = "<div class=\"bg-warning message text-center \">301</div>" ;
 		} else {
 			$site_en_ligne = "<div class=\"bg-danger message text-center \">ERROR</div>" ;
 		}
@@ -49,8 +57,8 @@ function checkingSite ($data) {
 		$url_contenu = file_get_contents($url) ;
 		if (strpos($url_contenu, 'noindex') !== false) {
  			$meta_robot_friendly = "<div class=\"bg-danger message text-center \">Noindex !</div>" ;
-		} else 
-{			$meta_robot_friendly = "<div class=\"bg-success message text-center \">OK</div>" ;
+		} else {
+			$meta_robot_friendly = "<div class=\"bg-success message text-center \">OK</div>" ;
 		}
 
 		// ====================================================================================
@@ -58,7 +66,5 @@ function checkingSite ($data) {
 		echo '<th scope="row">',$url,'</th><td>',$site_en_ligne,'</td><td>',$robots_txt_friendly,'</td><td>',$meta_robot_friendly,'</td></tr>' ;
 
 	}
-
 }
-
 ?>
